ece361cceb49e07762b84c065e9459e735a5bf45
Dat Le <datlt@antsomi.com>
2026-01-17 21:29:12 +0700

    update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -7,6 +7,7 @@ const DEV_TOKEN = 'q7YVQIHsOVWkbqJwzuy7tuNlbdHrrRkcH9hbdeJdpj1eP6051J'
 export const api = ky.create({
   prefixUrl: import.meta.env.VITE_API_URL || 'http://localhost:4000/api',
   timeout: 30000,
+  retry: 0,
   hooks: {
     beforeRequest: [
       (request) => {
